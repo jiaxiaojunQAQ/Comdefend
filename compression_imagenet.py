a2ed4d5f7f942d9dbdac93d53cde841065ea4099
jiaxiaojunQAQ <1642768580@qq.com>
2019-03-30 10:13:57 +0800

    提交文件

---
+++ compression_imagenet.py
@@ -0,0 +1,119 @@
+import sys
+from processer import *
+import numpy as np
+import canton as ct
+from canton import *
+import tensorflow as tf
+import time
+import os
+import math
+import cv2
+def mkdir(path):
+    # 引入模块
+    import os
+    isExists = os.path.exists(path)
+    if not isExists:
+        os.makedirs(path)
+        print('make')
+def ComCNN():
+    c=Can()
+    def conv(nip,nop,flag=True):
+        c.add(Conv2D(nip,nop,k=3,usebias=True))
+        if flag:
+            c.add(Act('elu'))
+    c.add(Lambda(lambda x:x-0.5))
+    conv(3,16)
+    conv(16,32)
+    conv(32,64)
+    conv(64,128)
+    conv(128,256)
+    conv(256,128)
+    conv(128,64)
+    conv(64,32)
+    conv(32,12,flag=False)
+    c.chain()
+    return c
+
+def RecCNN():
+    c=Can()
+    def conv(nip,nop,flag=True):
+        c.add(Conv2D(nip,nop,k=3,usebias=True))
+        if flag:
+            c.add(Act('elu'))
+    conv(12,32)
+    conv(32,64)
+    conv(64,128)
+    conv(128,256)
+    conv(256,128)
+    conv(128,64)
+    conv(64,32)
+    conv(32,16)
+    conv(16,3,flag=False)
+    c.add(Act('sigmoid'))
+    c.chain()
+    return c
+
+def get_defense():
+    x = ph([None,None,3])
+    x = tf.clip_by_value(x,clip_value_max=1.,clip_value_min=0.)
+    code_noise = tf.Variable(1.0)
+    linear_code = com(x)
+    noisy_code = linear_code - \
+        tf.random_normal(stddev=code_noise,shape=tf.shape(linear_code))
+    binary_code = Act('sigmoid')(noisy_code)
+    y = rec(binary_code)
+    set_training_state(False)
+    quantization_threshold = tf.Variable(0.5)
+    binary_code_test = tf.cast(binary_code>quantization_threshold,tf.float32)
+    y_test = rec(binary_code_test)
+    def test(batch,quanth):
+        sess = ct.get_session()
+        res = sess.run([binary_code_test,y_test,binary_code,y,x],feed_dict={
+            x:batch,
+            quantization_threshold:quanth,
+        })
+        return res
+    return test
+
+def Compression(path,path1,threshold=.5): #将路径中图像压缩还原并保存再路径中 code bool code2 float
+    import cv2
+    image=readimage(path)
+    minibatch =[image]
+    minibatch=np.array(minibatch)
+    #print(minibatch.shape)
+    code, rec, code2, rec2,x= test(minibatch,threshold)
+    img2=change(rec)
+    cv2.imwrite(path1,img2)
+
+    return code, code2
+def load():
+    print("****")
+    com.load_weights('enc20_0.0001.npy')
+    rec.load_weights('dec20_0.0001.npy')
+com,rec = ComCNN(),RecCNN()
+com.summary()
+rec.summary()
+import time
+if __name__ == '__main__':
+    test = get_defense()
+    get_session().run(ct.gvi())
+    load()
+    start = time.time()
+    path='adv.png'
+    path3="temp_imagenet/"
+    mkdir(path3)
+    Divided_Pach(path,path3)
+    for i in range(1,50):
+            mkdir("temp_imagenet/")
+            mkdir("com_imagenet_temp/")
+            path1="temp_imagenet/"+str(i)+'.png'
+            path2="com_imagenet_temp/"+str(i)+'.png'
+            print(path2)
+            Compression(path1,path2)
+    concated=mergeimage('com_imagenet_temp/')
+        #print(concated.shape)
+    path4='result.png'
+    print(path4)
+    cv2.imwrite(path4,concated)
+    end = time.time()
+print(end-start)
---
+++ processer.py
@@ -0,0 +1,56 @@
+import numpy as np
+import math
+import cv2
+def change(x):
+    x *=255
+    x=x.astype('uint8')
+    img=x[0]
+    return img
+def psnr(im1,im2):
+    mse = np.mean( (im1 - im2) ** 2 )
+    if mse == 0:
+        return 100
+    PIXEL_MAX = 255.0
+    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
+def readimage(path):
+        result=cv2.imread(path)
+        result = result.astype('float32')
+        result/=255
+        return result
+def mergeimage_column(path,n):
+    path1=path+str(n)+'.png'
+    t1=cv2.imread(path1)
+    for i in range(1,7):
+        path2=path+str(n+i)+".png"
+        t2=cv2.imread(path2)
+        t1 = np.hstack((t1,t2))
+    return t1
+def mergeimage(path):
+    t1=mergeimage_column(path,1)
+    print(t1.shape)
+    for  i in range(1,7):
+         print(1+7*i)
+         t2=mergeimage_column(path,1+7*i)
+         t1 = np.vstack((t1,t2))
+    return t1
+def Divided_Pach(path1,path2):
+    lena=cv2.imread(path1)
+    height= 224
+    width=224
+    a=0
+    b=0
+    count=0
+    print(width)
+    print(height)
+    path=path2
+    while a<height:
+            while b<width:
+                box = lena[a:a+32,b:b+32]
+                count=count+1
+                b=b+32
+                path2=path2+str(count)+".png"
+                cv2.imwrite(path2,box)
+                path2=path
+            b=0
+            a=a+32
+    print(count)
