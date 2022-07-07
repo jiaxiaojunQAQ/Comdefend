6cd8b4e65ebf6dc63a519b95c235917cd4ab59d2
jiaxiaojunQAQ <1642768580@qq.com>
2022-07-07 17:06:31 +0800

    models

---
+++ models/celeba/vgg.py
@@ -0,0 +1,93 @@
+'''VGG for CIFAR10. FC layers are removed.
+(c) YANG, Wei 
+'''
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+import math
+
+
+
+
+class VGG(nn.Module):
+
+    def __init__(self, num_classes=2):
+        super(VGG, self).__init__()
+        self.conv1_1 = nn.Conv2d(3, 64, 3, padding=1)
+        self.conv1_2 = nn.Conv2d(64, 64, 3, padding=1)
+
+        self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv2_2 = nn.Conv2d(128, 128, 3, padding=1)
+
+        self.conv3_1 = nn.Conv2d(128, 256, 3, padding=1)
+        self.conv3_2 = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv3_3 = nn.Conv2d(256, 256, 3, padding=1)
+
+        self.conv4_1 = nn.Conv2d(256, 512, 3, padding=1)
+        self.conv4_2 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv4_3 = nn.Conv2d(512, 512, 3, padding=1)
+
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+        self.maxpool4 = nn.MaxPool2d(2, stride=1, ceil_mode=True)
+
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 2),
+        )
+
+        self._initialize_weights()
+
+    def forward(self, x):
+        conv1_1 = self.relu(self.conv1_1(x))
+        conv1_2 = self.relu(self.conv1_2(conv1_1))
+        pool1   = self.maxpool(conv1_2)
+
+        conv2_1 = self.relu(self.conv2_1(pool1))
+        conv2_2 = self.relu(self.conv2_2(conv2_1))
+        pool2   = self.maxpool(conv2_2)
+
+        conv3_1 = self.relu(self.conv3_1(pool2))
+        conv3_2 = self.relu(self.conv3_2(conv3_1))
+        conv3_3 = self.relu(self.conv3_3(conv3_2))
+        pool3   = self.maxpool(conv3_3)
+
+        conv4_1 = self.relu(self.conv4_1(pool3))
+        conv4_2 = self.relu(self.conv4_2(conv4_1))
+        conv4_3 = self.relu(self.conv4_3(conv4_2))
+        x   = self.maxpool4(conv4_3)
+
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                n = m.weight.size(1)
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+
+
+def vgg(**kwargs):
+    """VGG 11-layer model (configuration "A")
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = VGG()
+    return model
+
